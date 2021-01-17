c4a36a77d7d669c91b19a994a07c165eca3389d4
Austin Kurilla <auggie124@gmail.com>
2021-01-16 23:16:48 -0600

    Did some work
    -Navbar
    -Logo

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function Show() {
+var navList = document.getElementById("nav-lists");
+navList.classList.add("_Menus-show");
+}
+
+function Hide(){
+var navList = document.getElementById("nav-lists");
+navList.classList.remove("_Menus-show");
+}
