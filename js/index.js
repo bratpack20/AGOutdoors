c6ed59c76ebcba56864695ec2582bb0dad97435c
bratpack20 <61706227+bratpack20@users.noreply.github.com>
2024-02-26 17:36:25 -0600

    Delete js directory

--- js/index.js
+++
@@ -1,9 +0,0 @@
-function Show() {
-var navList = document.getElementById("nav-lists");
-navList.classList.add("_Menus-show");
-}
-
-function Hide(){
-var navList = document.getElementById("nav-lists");
-navList.classList.remove("_Menus-show");
-}
